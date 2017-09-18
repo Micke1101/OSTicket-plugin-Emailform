7845c20a77f90a8686af9a8e50e312114f336a1c
Micke1101 <micaelsiphone@gmail.com>
2017-09-18 23:08:19 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 require_once INCLUDE_DIR . 'class.plugin.php';
 
-class TinyMCEPluginConfig extends PluginConfig
+class EmailformPluginConfig extends PluginConfig
 {
 
     // Provide compatibility function for versions of osTicket prior to
@@ -18,7 +18,7 @@ class TinyMCEPluginConfig extends PluginConfig
                 }
             );
         }
-        return Plugin::translate('tinymce');
+        return Plugin::translate('emailform');
     }
 
     /**
@@ -31,110 +31,37 @@ class TinyMCEPluginConfig extends PluginConfig
     function getOptions()
     {
         list ($__, $_N) = self::translate();
-        return array(
-            'mainoptions' => new SectionBreakField([
-                'label' => $__('Main options'),
-                'hint' => $__('Mainly visual settings.'),
-                'default' => TRUE
-            ]),
-            'plugins' => new ChoiceField([
-                'label' => $__('Plugins'),
-                'required' => false,
-                'hint' => $__('What plugins do you want to load.'),
+		$choices = array();
+		foreach (DynamicForm::objects()
+				->filter(array('type'=>'G'))
+				->exclude(array('flags__hasbit' => DynamicForm::FLAG_DELETED))
+				->order_by('title') as $form){
+					$choices[$form->get('id')] = $form->get('title');
+				}
+		$fields = array();
+		if (Config::exists('emailform-defaúlt-form') && ($form = DynamicForm::lookup(Config::get('emailform-defaúlt-form')))){
+			foreach ($form->getDynamicFields() as $field){
+				$fields['emailform-' . $field->get('name')] = new TextboxField([
+					'label' => $__($field->get('label')),
+					'required'=>true,
+					'hint' => $__('Regular expression to capture this variable'),
+					'default' => '',
+					'configuration' => array(
+						'size' => 40,
+						'length' => 100
+					)
+				]);
+			}
+		}
+        return array_merge(array(
+            'emailform-defaúlt-form' => new ChoiceField([
+                'label' => $__('Email form'),
+				'required' => false,
+                'hint' => $__('Select what form shall be added to all emails'),
                 'default' => '',
-                'configuration'=>array('multiselect'=>true,'prompt'=>__('Plugins')),
-                'choices' => array(
-                    'advlist' => __('Advanced list'),
-                    'autolink' => __('Autolink'),
-                    'lists' => __('Normalize lists'),
-                    'link' => __('Links'),
-                    'image' => __('Images'),
-                    'charmap' => __('Unicode characters'),
-                    'print' => __('Print'),
-                    'preview' => __('Preview'),
-                    'anchor' => __('Anchor'),
-                    'textcolor' => __('Text color'),
-                    'searchreplace' => __('Search and Replace'),
-                    'visualblocks' => __('Visual Blocks'),
-                    'code' => __('Edit HTML'),
-                    'fullscreen' => __('Fullscreen'),
-                    'insertdatetime' => __('Date and time'),
-                    'media' => __('HTML5 Media'),
-                    'table' => __('Table'),
-                    'contextmenu' => __('Context menu'),
-                    'paste' => __('Filter office content'),
-                    'help' => __('Help'),
-                )
-            ]),
-            'toolbar' => new TextboxField([
-                'label' => $__('Toolbar'),
-                'required' => false,
-                'configuration'=>array('size'=>50,'length'=>1024),
-                'hint' => sprintf($__('How do you want your toolbar to look like, %s'), '<a href="https://www.tinymce.com/docs/configure/editor-appearance/#toolbar">TinyMCE</a>'),
-                'default' => 'insert | undo redo |  styleselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
-            ]),
-            'theme' => new ChoiceField([
-                'label' => $__('Theme'),
-                'required' => true,
-                'hint' => $__('What theme do you want to use.'),
-                'default' => 'modern',
-                'choices' => array(
-                    'modern' => __('Modern'),
-                )
-            ]),
-            'menubar' => new BooleanField([
-                'label' => $__('Show menubar'),
-                'required' => false,
-                'hint' => $__('Display the menubar or not.'),
-                'default' => true
-            ]),
-            'poweredby' => new BooleanField([
-                'label' => $__('Show powered by message'),
-                'required' => false,
-                'hint' => $__('Display the powered by message or not.'),
-                'default' => true
-            ]),
-            'height' => new TextboxField([
-                'label' => $__('Height'),
-                'required' => true,
-                'size'=>16,
-                'validator' => 'number',
-                'hint' => $__('The default height of TinyMCE'),
-                'default' => '250'
-            ]),
-            'autosaveoptions' => new SectionBreakField([
-                'label' => $__('Autosave options'),
-                'hint' => $__('The options regarding autosaving/drafts.'),
-                'default' => TRUE
-            ]),
-            'doautosave' => new BooleanField([
-                'label' => $__('Enable autosaving'),
-                'required' => false,
-                'hint' => $__('TinyMCE will create drafts automaticly.'),
-                'default' => true
-            ]),
-            'autosaveinterval' => new TextboxField([
-                'label' => $__('Autosave frequency'),
-                'required' => false,
-                'size'=>16,
-                'validator' => 'number',
-                'hint' => $__('How long between each save in seconds.'),
-                'default' => '30'
-            ]),
-            'tryrestoreempty' => new BooleanField([
-                'label' => $__('Restore when empty'),
-                'required' => false,
-                'hint' => $__('If the user has a draft available restore it automaticly.'),
-                'default' => true
-            ]),
-            'autosaveretention' => new TextboxField([
-                'label' => $__('Draft lifespan'),
-                'required' => false,
-                'size'=>16,
-                'validator' => 'number',
-                'hint' => $__('How long should a draft be stored for in minutes.'),
-                'default' => '30'
-            ]),
-        );
+				'choices' => $choices
+            ])
+        
+        ), $fields);
     }
 }
